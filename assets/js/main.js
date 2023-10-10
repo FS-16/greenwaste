498bdc75e3d580a0fadd53b1be9aaeeb9b1d2a30
koreoxy <rayssankn@gmail.com>
2023-10-10 22:21:18 +0700

    feat : add page ask question dan loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+
+  loader.classList.add('loader-hidden');
+  loader.addEventListener('transition', () => {
+    document.body.removeChild('loader');
+  });
+});
